f0853cadce105c19779ce63d91f5ea83a7442b9b
Liz Kaufman <lizkaufman92@gmail.com>
2021-12-04 22:04:41 +0000

    day 2 pt 2 done

--- day02/index.js
+++ day02/index.js
@@ -33,18 +33,41 @@ function pilotSub(commands) {
   return finalPosition.horizontal * finalPosition.depth;
 }
 
-console.log(pilotSub(data));
-
-// const partOneAnswer = pilotSub(data);
-// console.log({ partOneAnswer });
+// console.log(pilotSub(data));
 
 // PART TWO:
 
 /*
-- 
+- down = increase aim by number
+- up = decrease aim by number
+- forward = increase horizontal by number AND increase depth by aim * number
 */
 
-// const partTwoAnswer = ;
-// console.log({ partTwoAnswer });
+function pilotSubAim(commands) {
+  const finalPosition = commands.reduce(
+    (acc, cur) => {
+      const direction = cur.charAt(0);
+      const distance = Number(cur.split(" ")[1]);
+      switch (direction) {
+        case "d":
+          return { ...acc, aim: acc.aim + distance };
+        case "u":
+          return { ...acc, aim: acc.aim - distance };
+        case "f":
+          return {
+            ...acc,
+            horizontal: acc.horizontal + distance,
+            depth: acc.depth + acc.aim * distance,
+          };
+        default:
+          return acc;
+      }
+    },
+    { horizontal: 0, depth: 0, aim: 0 }
+  );
+  return finalPosition.horizontal * finalPosition.depth;
+}
 
-module.exports = { pilotSub };
+// console.log(pilotSubAim(data));
+
+module.exports = { pilotSub, pilotSubAim };
--- tests/02.test.js
+++ tests/02.test.js
@@ -1,9 +1,15 @@
 const { test, expect } = require("@jest/globals");
 const { example } = require("../day02/data.js");
-const { pilotSub } = require("../day02/index.js");
+const { pilotSub, pilotSubAim } = require("../day02/index.js");
 
 test("When called w/ example data, pilotSub should return 150", () => {
   const expected = 150;
   const actual = pilotSub(example);
+  expect(actual).toBe(expected);
+});
+
+test("When called w/ example data, pilotSubAim should return 900", () => {
+  const expected = 900;
+  const actual = pilotSubAim(example);
   expect(actual).toBe(expected);
 });
